c7ac9368d68c944bca0431a2cbf79c958a2182c2
BranislavMacosinec <branomps@gmail.com>
2017-06-13 18:55:02 +0100

    Class ProductContext Done.

---
+++ JoyTea/Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JoyTea.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("JoyTea")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set;  }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
