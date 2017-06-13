328e7d6bc17336e551cb92b1a41ce521b263300a
BranislavMacosinec <branomps@gmail.com>
2017-06-13 20:09:09 +0100

    Change a value in Data Anotation in Product class on Property Description from StringLength  value 1000 to 2500.

--- JoyTea/Models/Product.cs
+++ JoyTea/Models/Product.cs
@@ -14,7 +14,7 @@ namespace JoyTea.Models
         [Required, StringLength(100), Display(Name = "Name")]
         public string ProductName { get; set; }
 
-        [Required, StringLength(1000), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        [Required, StringLength(2500), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
         public string ImagePath { get; set; }
