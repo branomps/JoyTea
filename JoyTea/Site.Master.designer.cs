b80c5679df2ed573ac214951f5cf0a003d786251
BranislavMacosinec <branomps@gmail.com>
2017-06-15 18:37:12 +0100

    Add logo image to solution and add a anchor to boostrap navbar.

--- JoyTea/Site.Master.designer.cs
+++ JoyTea/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace JoyTea {
     public partial class SiteMaster {
         
         /// <summary>
+        /// Logo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Logo;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
