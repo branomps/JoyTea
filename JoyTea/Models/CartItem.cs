24d61a2df4e55d51490ab1997efdc4783620ecf7
BranislavMacosinec <branomps@gmail.com>
2017-06-16 18:09:03 +0100

    create ShoppingCartAction class and ad logic for it.

--- JoyTea/Models/CartItem.cs
+++ JoyTea/Models/CartItem.cs
@@ -10,9 +10,9 @@ namespace JoyTea.Models
     {
         [Key]
         public string ItemId { get; set; }
-        public string Cartid { get; set; }
+        public string CartId { get; set; }
         public int Quantity { get; set; }
-        public System.DateTime DataCreated { get; set; }
+        public System.DateTime DateCreated { get; set; }
         public int ProductId { get; set; }
 
         public virtual Product Product { get; set; }
